07fca3c7b9a27362c4d17fd1fbf78ecdbf952dba
Mikuláš Vanoušek <mikulas.vanousek@juicymo.cz>
2019-07-10 21:33:45 +0200

    Car is maybe moveable.

--- Model/Map.py
+++ Model/Map.py
@@ -11,10 +11,21 @@ class Vector:
     def plus(self, anotherVector):
         return Vector(self.x + anotherVector.x, self.y + anotherVector.y)
 
+    def __eq__(self, o: object) -> bool:
+        if not isinstance(o, Vector):
+            return False
+        return o.x == self.x and o.y == self.y
+
+
 class Road:
     def __init__(self, start, end):
         self.start = start
         self.end = end
+
+    def __eq__(self, o: object) -> bool:
+        if not isinstance(o, Road):
+            return False;
+        return self.start == o.start and self.end == o.end or self.start == o.end and self.end == o.start
 
 
 class Car:
@@ -54,3 +65,12 @@ class Map:
 
     def getRandomVectorOnMap(self):
         return Vector(random.randint(0, self.size.x), random.randint(0, self.size.y))
+
+    def isRoad(self, position: Vector, direction: Vector) -> bool:
+        road = Road(position, position.plus(direction))
+        return road in self.roads
+
+    def moveCar(self, move: Vector):
+        move = Vector(1, 0)
+        if self.isRoad(self.car.position, move):
+            self.car.position = self.car.position.plus(move)
