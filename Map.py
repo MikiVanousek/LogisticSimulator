98f288c05190e4ea33930bb23202cb38c259c8d2
Mikuláš Vanoušek <mikulas.vanousek@juicymo.cz>
2019-07-10 16:16:56 +0200

    Created Vector, Road...

--- Map.py
+++
@@ -1,9 +0,0 @@
-class Map:
-    width = 0
-    height = 0
-
-    def __init__(self, w, h):
-        self.width = w
-        self.height = h
-
-
---
+++ Model/Map.py
@@ -0,0 +1,40 @@
+class Vector:
+    x = 0
+    y = 0
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Road:
+    def __init__(self, startVector, roadVector):
+        self.start = startVector
+        self.road = roadVector
+
+
+class Map:
+    size = Vector(0, 0)
+    roads = []
+
+    def __init__(self, w, h):
+        size = Vector(w, h)
+
+    def fillWithRoads(self):
+        up = Vector(1, 0)
+        down = Vector(-1, 0)
+        right = Vector(0, 1)
+        left = Vector(0, -1)
+        for x in range(self.size.x):
+            for y in range(self.size.y):
+                position = Vector(x, y)
+
+                if x != self.size.x:
+                    self.roads.append(Road(position, up))
+                if x != 0:
+                    self.roads.append(Road(position, down))
+                if y != self.size.y:
+                    self.roads.append(Road(position, right))
+                if y != 0:
+                    self.roads.append(Road(position, left))
+
